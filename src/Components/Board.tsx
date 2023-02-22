46aa89f1cd0bf27281b32fded8ca5dd4b0a71ed8
ByulKim <byulkim1225@gmail.com>
2023-02-22 20:47:27 +0900

    style 작업중

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -4,7 +4,7 @@ import DraggableCard from './DraggableCard';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-    padding: 20px 10px 20px 10px;
+    padding-top: 10px;
     background-color: ${(props) => props.theme.boardColor};
     border-radius: 5px;
     min-height: 300px;
@@ -25,7 +25,8 @@ interface IAreaProps{
 }
 
 const Area = styled.div<IAreaProps>`
-    background-color: ${props => props.isDraggingOver ? "pink" : props.isDraggingFromThisWith? "red" : "blue"};
+    padding: 20px;
+    background-color: ${props => props.isDraggingOver ? "#dfe6e9" : props.isDraggingFromThisWith? "#b2bec3" : "transparent"};
     flex-grow: 1;
     transition: background-color .3s ease-in-out;
 `
@@ -40,11 +41,11 @@ function Board({toDos, boardId}: IBoardProps) {
         <Wrapper>
             <Title>{boardId}</Title>
             <Droppable droppableId={boardId}>
-                {(provided, snapthot) => (
+                {(provided, snapshot) => (
                 <Area 
                 //snapthot을 통해 드래해서 board로 들어오고 떠났는지 여부를 styled 컴포넌트로 넘긴다.
-                isDraggingOver={snapthot.isDraggingOver} 
-                isDraggingFromThisWith={Boolean(snapthot.draggingFromThisWith)}
+                isDraggingOver={snapshot.isDraggingOver} 
+                isDraggingFromThisWith={Boolean(snapshot.draggingFromThisWith)}
                 ref={provided.innerRef} 
                 {...provided.droppableProps}>
                     {toDos.map((toDo, index) => (
